f8d15f606fbf7f2c4bf83007e560f8d0573e5e11
Julius de Boer <45075461+JuliusDeBoer@users.noreply.github.com>
2024-04-16 09:18:57 +0200

    feat: Drone go brrrr

--- tello.py
+++ tello.py
@@ -1,7 +1,17 @@
 #!/bin/python3
+from djitellopy import Tello
 
 def main():
-    print("Hello, World!")
+    tello = Tello()
+
+    tello.connect()
+    tello.takeoff()
+
+    tello.move_left(100)
+    tello.rotate_counter_clockwise(90)
+    tello.move_forward(100)
+
+    tello.land()
 
 if __name__ == "__main__":
     main()
